057d9d2208664d776b318a6cd3152dfa0dc4023b
3n2c <3n2c@users.noreply.github.com>
2024-06-12 18:29:36 +0530

    Branch Shahmukhi created for Punjabi Shahmukhi
    	- All Shahmukhi books
    	- NCERT - Urdu books
    	- Urdu Qaida - Hazara Singh
    	- Urdu Qaida - Amarwant Singh

---
+++ Shahmukhi/Shahmukhi.md
@@ -0,0 +1,5 @@
+[Pradeep Punjabi Urdu Teacher](http://www.panjabdigilib.org/webuser/searches/displayPage.jsp?ID=41482&page=1&CategoryID=12&Searched=)
+https://www.travel-culture.com/images/pctb/Urdu%201.pdf
+https://www.travel-culture.com/images/pctb/Urdu%202%20Full%20Book%20for%20Online.pdf
+
+[Gurmukhi Shahmukhi Premier](http://hazarasinghprofessor.org/urdu%20premier/page2.html)
---
+++ Shahmukhi/hazara/html.sh
@@ -0,0 +1,91 @@
+wget https://hazarasinghprofessor.org/urdu%20premier/page1_html_42be7392.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/page1_html_514ac682.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/page1_html_m4d58b123.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/page1_html_m3ae51936.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/page2_html_m1b2bde75.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/page3_html_1b104f38.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/page4_html_7b4b8aff.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/page5_html_66dbb1d0.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/page5_html_42173d0a.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/page5_html_m1c68c6e3.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE6_html_m1570fef4.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE6_html_m7aaed4ca.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE6_html_198a4499.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE7_html_11e6c738.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE7_html_40410bdb.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE7_html_70ab1efc.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE7_html_m7f4a46d9.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE8_html_m3b8aacbe.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE8_html_b2a505d.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE8_html_4f798a46.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_m2af912e6.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_5c013d1f.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_6148aefe.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_m7371cc6a.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_m30d6009f.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_m6e5efb57.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%209_html_m12035c1c.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_16129963.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_67f7ee57.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_a050616.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_43064e2b.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_m36903ee9.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE010_html_2936cd61.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20011_html_6b2efdd9.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20011_html_7c7477e.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20012_html_3a782e5.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_372fa174.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_45a19196.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_m7cbc9776.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_5a9ab9c3.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_m13cd322.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_1efe4dba.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_28648036.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20013_html_mb1f0b18.jpg
+
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20014_html_m7287b315.jpg
+wget https://hazarasinghprofessor.org/urdu%20premier/PAGE%20014_html_m2df6e31c.jpg
+
+
+# mv PAGE\ 011_html_6b2efdd9.jpg  PAGE011_html_6b2efdd9.jpg
+# mv PAGE\ 011_html_7c7477e.jpg   PAGE011_html_7c7477e.jpg
+# mv PAGE\ 012_html_3a782e5.jpg   PAGE012_html_3a782e5.jpg
+# mv PAGE\ 013_html_1efe4dba.jpg  PAGE013_html_1efe4dba.jpg
+# mv PAGE\ 013_html_28648036.jpg  PAGE013_html_28648036.jpg
+# mv PAGE\ 013_html_372fa174.jpg  PAGE013_html_372fa174.jpg
+# mv PAGE\ 013_html_45a19196.jpg  PAGE013_html_45a19196.jpg
+# mv PAGE\ 013_html_5a9ab9c3.jpg  PAGE013_html_5a9ab9c3.jpg
+# mv PAGE\ 013_html_m13cd322.jpg  PAGE013_html_m13cd322.jpg
+# mv PAGE\ 013_html_m7cbc9776.jpg PAGE013_html_m7cbc9776.jpg
+# mv PAGE\ 013_html_mb1f0b18.jpg  PAGE013_html_mb1f0b18.jpg
+# mv PAGE\ 014_html_m2df6e31c.jpg PAGE014_html_m2df6e31c.jpg
+# mv PAGE\ 014_html_m7287b315.jpg PAGE014_html_m7287b315.jpg
+# mv PAGE\ 9_html_5c013d1f.jpg  PAGE9_html_5c013d1f.jpg
+# mv PAGE\ 9_html_6148aefe.jpg  PAGE9_html_6148aefe.jpg
+# mv PAGE\ 9_html_m12035c1c.jpg PAGE9_html_m12035c1c.jpg
+# mv PAGE\ 9_html_m2af912e6.jpg PAGE9_html_m2af912e6.jpg
+# mv PAGE\ 9_html_m30d6009f.jpg PAGE9_html_m30d6009f.jpg
+# mv PAGE\ 9_html_m6e5efb57.jpg PAGE9_html_m6e5efb57.jpg
+# mv PAGE\ 9_html_m7371cc6a.jpg PAGE9_html_m7371cc6a.jpg
+
+
+
+
+
+
+
+
+
+
